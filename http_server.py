52996d6d6f57c0ff33b1cdac5ec215db45eb704f
Eloy <zer1t0ps@protonmail.com>
2020-04-09 08:42:58 +0200

    add parse_parameters

--- http_server.py
+++ http_server.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 from urllib.parse import urlparse, parse_qs
 import base64
 import logging
 import argparse
 import os
+import json
 from abc import ABC, abstractmethod
+from functools import partial
 
 logger = logging.getLogger(__name__)
 
 DEFAULT_PORT = 8000
 DEFAULT_ADDRESS = "0.0.0.0"
+DEFAULT_DIR = os.getcwd()
 
 
 def parse_args():
@@ -33,6 +36,14 @@ def parse_args():
     )
 
     parser.add_argument(
+        "--directory", "-d",
+        default=DEFAULT_DIR,
+        help="Specify alternative directory [default: {}]".format(
+            DEFAULT_DIR
+        )
+    )
+
+    parser.add_argument(
         "--debug",
         required=False,
         action="store_true"
@@ -49,13 +60,15 @@ def main():
     port = args.port
     address = args.address
 
-    httpd = HTTPServer(
+    httpd = ThreadingHTTPServer(
         (address, port),
-        CustomHttpRequestHandler
+        partial(CustomHttpRequestHandler, directory=args.directory)
     )
-    print("Serving HTTP on {0} port {1} (http://{0}:{1}/) ...".format(
-        address, port
-    ))
+    print(
+        "Serving HTTP on {host} port {port} (http://{host}:{port}/) ...".format(
+            host=address, port=port
+        )
+    )
 
     try:
         httpd.serve_forever()
@@ -68,7 +81,7 @@ def main():
     logging.info('Stop\n')
 
 
-class CustomHttpRequestHandler(BaseHTTPRequestHandler):
+class CustomHttpRequestHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
         logging.debug("GET")
@@ -78,21 +91,71 @@ class CustomHttpRequestHandler(BaseHTTPRequestHandler):
         logging.debug("POST")
         self._handle_request()
 
+    def do_HEAD(self):
+        logging.debug("HEAD")
+        self._handle_request()
+
+    def do_PUT(self):
+        logging.debug("PUT")
+        self._handle_request()
+
     def _handle_request(self):
-        request_info = RequestInfo(
+        request_info = self.parse_request()
+        parameters = self.parse_parameters(request_info)
+
+        try:
+            self.execute_action(parameters)
+        except FileNotFoundError as ex:
+            logger.debug("File not found: %s", ex)
+            self.send_error(404)
+        except IsADirectoryError as ex:
+            logger.debug("Directory requested: %s", ex)
+            self.send_error(404)
+
+    def parse_request(self):
+        return RequestInfo(
             method=self.command,
-            url=self.path
+            url=self.path,
+            headers=self.headers,
+            rfile=self.rfile
         )
 
+    def parse_parameters(self, request_info):
+        parameters = Parameters.from_method(request_info.method)
+        parameters.update(Parameters.from_path(request_info.url.path))
+        parameters.update(
+            Parameters.from_dictionary(request_info.url.query_string)
+        )
+
+        content_length = request_info.content_length
+        if content_length > 0:
+            content = request_info.rfile.read(content_length)
+            if request_info.content_type == ContentType.URLENCODED:
+                content_params = Parameters.from_dictionary(
+                    parse_qs(content)
+                )
+            elif request_info.content_type == ContentType.JSON:
+                content_params = Parameters.from_dictionary(
+                    json.loads(content)
+                )
+            else:
+                content_params = Parameters.from_raw_data(content)
+
+            parameters.update(content_params)
+
+        parameters.path = self.translate_path(parameters.path)
+
+        return parameters
+
     def execute_action(self, parameters):
-        if parameters.action == "download_file":
+        if parameters.action == Actions.DOWNLOAD_FILE:
             self.download_file(
                 parameters.path,
                 parameters.offset,
                 parameters.size,
                 EncoderFactory.create(parameters.encoding)
             )
-        elif parameters.action == "upload_file":
+        elif parameters.action == Actions.UPLOAD_FILE:
             self.upload_file(
                 parameters.path,
                 parameters.data,
@@ -101,28 +164,31 @@ class CustomHttpRequestHandler(BaseHTTPRequestHandler):
             )
 
     def download_file(self, path, offset, size, encoder):
-        try:
-            self.wfile.write(self.read_file(path, offset, size, encoder))
-            self.send_response(200)
-            self.send_header("Content-type", "application/octet-stream")
-            self.end_headers()
-        except FileNotFoundError as ex:
-            logger.debug("File not found: %s", ex)
-            self.send_error(404)
+        self.wfile.write(self.read_file(path, offset, size, encoder))
+        self.send_response(200)
+        self.send_header("Content-type", ContentType.OCTET_STREAM)
+        self.end_headers()
 
     def upload_file(self, path, data, append, encoder):
-        try:
-            self.write_file(path, data, append, encoder)
-            self.send_response(200)
-            self.end_headers()
-        except FileNotFoundError as ex:
-            logger.debug("File not found: %s", ex)
-            self.send_error(404)
+        self.write_file_and_dirs(path, data, append, encoder)
+        self.send_response(200)
+        self.end_headers()
 
     def read_file(self, path, offset, size, encoder):
         with open(path, "rb") as f:
             f.seek(offset)
             return encoder.encode(f.read(size))
+
+    def write_file_and_dirs(self, path, data, append, encoder):
+        try:
+            self.write_file(path, data, append, encoder)
+        except FileNotFoundError:
+            self.create_dirs(path)
+            self.write_file(path, data, append, encoder)
+
+    def create_dirs(self, path):
+        dir_path = os.path.dirname(path)
+        os.makedirs(dir_path)
 
     def write_file(self, path, data, append, encoder):
         mode = "ab" if append else "wb"
@@ -143,48 +209,28 @@ class CustomHttpRequestHandler(BaseHTTPRequestHandler):
         except IOError:
             self.send_error(404)
 
-    def _handle_post(self, url):
 
-        if url.action == "af":
-            logging.debug("Appending to file")
-            self.append_raw_data_to_file(url)
-        elif url.path == "/b64" or url.action == "b64":
-            logging.debug("Appending to file base64")
-            self.append_base64_data_to_file(url)
-        else:
-            logging.debug("Show content")
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            print(post_data)
-
-        self.send_response(200)
-        self.end_headers()
-
-    def append_base64_data_to_file(self, url):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        file_name = os.path.basename(url.args["file"][0])
-
-        raw_data = base64.b64decode(post_data)
-
-        self.append_data_to_file(file_name, raw_data)
-
-    def append_raw_data_to_file(self, url):
-        content_length = int(self.headers['Content-Length'])
-        raw_data = self.rfile.read(content_length)
-        file_name = os.path.basename(url.args["file"][0])
-        self.append_data_to_file(file_name, raw_data)
-
-    def append_data_to_file(self, filename, data):
-        with open(filename, "ab") as f:
-            f.write(data)
+class ContentType:
+    JSON = "application/json"
+    URLENCODED = "application/x-www-form-urlencoded"
+    OCTET_STREAM = "application/octet-stream"
 
 
 class RequestInfo(object):
 
-    def __init__(self, method, url):
+    def __init__(self, method, url, headers, rfile):
         self.method = method
         self.url = Url(url)
+        self.headers = headers
+        self.rfile = rfile
+
+    @property
+    def content_type(self):
+        return self.headers.get("Content-type", "")
+
+    @property
+    def content_length(self):
+        return int(self.headers.get("Conten-Length", "0"))
 
 
 class Url:
@@ -206,7 +252,16 @@ class Url:
 
     @property
     def args(self):
-        return parse_qs(self._url.query)
+        return self._parameters
+
+    @property
+    def query_string(self):
+        return self._parameters
+
+
+class Actions:
+    DOWNLOAD_FILE = "download_file"
+    UPLOAD_FILE = "upload_file"
 
 
 class Parameters:
@@ -229,6 +284,10 @@ class Parameters:
         self.append = append
         self.data = data
 
+    @property
+    def is_dir(self):
+        return os.path.isdir(self.path)
+
     def update(self, other):
         for key, value in other.__dict__.items():
             if value is not None:
@@ -236,17 +295,17 @@ class Parameters:
 
     @classmethod
     def from_method(cls, method):
-        if method == "POST":
-            return cls(action="upload_file")
+        if method == "POST" or method == "PUT":
+            return cls(action=Actions.UPLOAD_FILE)
         else:
-            return cls(action="download_file")
+            return cls(action=Actions.DOWNLOAD_FILE)
 
     @classmethod
     def from_path(cls, path):
         return cls(path=path)
 
     @classmethod
-    def from_query_string(cls, qs):
+    def from_dictionary(cls, qs):
         append = "append" in qs
         return cls(
             action=qs.get("action", None),
