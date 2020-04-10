bc91cabb695f118440ddd72bc865e74f14e05279
Eloy <zer1t0ps@protonmail.com>
2020-04-10 11:41:16 +0200

    add docstrings to encoders.py

--- httpserverlib/encoder.py
+++ httpserverlib/encoder.py
@@ -3,6 +3,8 @@ import base64
 
 
 class EncoderFactory(object):
+    """To create different encoders in function of the selecting encoding
+    """
 
     @classmethod
     def create(cls, encoding):
@@ -13,6 +15,7 @@ class EncoderFactory(object):
 
 
 class Encoder(ABC):
+    """Interface to define the methods provide for an encoder"""
 
     @abstractmethod
     def encode(self, data):
@@ -24,6 +27,9 @@ class Encoder(ABC):
 
 
 class EmptyEncoder(Encoder):
+    """Encoder that do not perform any encoding function, for compatibility
+    purposes.
+    """
 
     def encode(self, data):
         return data
@@ -33,15 +39,26 @@ class EmptyEncoder(Encoder):
 
 
 class Base64Encoder(Encoder):
+    """Encoder that performs base64 encoding and decoding. Supports URL base64
+    decoding.
+    """
 
     def encode(self, data):
+        """Encode binary data into base64, by using the regular dictionary"""
         return base64.b64encode(data)
 
     def decode(self, data):
+        """Decode base64 data. Supports the regular dictionary as well as url
+        base64.
+        """
         data = self._normalize_url_data(data)
         return base64.b64decode(data)
 
     def _normalize_url_data(self, data):
+        """Translate, if it is required, the url base64 dictionary, to the
+        regular one.
+        """
+
         missing_padding = len(data) % 4
         if missing_padding:
             data += b'=' * (4 - missing_padding)
