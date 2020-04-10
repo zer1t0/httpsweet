7519584ffea8495b11a583fbb7f57c2e24a2ce68
Eloy <zer1t0ps@protonmail.com>
2020-04-10 18:35:28 +0200

    add setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+name = "http_server"
+
+setuptools.setup(
+    name=name,
+    version="0.0.1",
+    author="Eloy Pérez González",
+    author_email="zer1t0ps@protonmail.com",
+    description="An HTTP server for download and upload files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/Zer1t0/" + name,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU AGPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
