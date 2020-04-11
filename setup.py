a3f551ca91cf9578eee4d84b06bd7a76a79ad540
Eloy <zer1t0ps@protonmail.com>
2020-04-11 13:16:45 +0200

    fix license classifier in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU AGPLv3 License",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
